6326c3d1da6b311b3eb0bee5e70c36ad2434fa37
GENIVANIA MACEDO OLIVEIRA <22282083@CFP127ALUNO.LOCAL>
2023-02-17 11:05:56 -0300

    falta o exame

--- app.js
+++ app.js
@@ -17,102 +17,99 @@ var entradaDados = readline.createInterface({
 
 //Entrada de dados do aluno e do professor:
 entradaDados.question('Nome do Aluno: \n', function (aluno) {
-
-    entradaDados.question('Sexo do aluno: [FEMININO | MASCULINO] \n', function (sexoAluno) {
-
-        entradaDados.question('Nome do Professor: \n', function (professor) {
-
-            entradaDados.question('Sexo do Professor: \n', function (sexoProfessor) {
-
-                entradaDados.question('Curso: \n', function (curso) {
-
-                    entradaDados.question('Disciplina: \n', function (disciplina) {
-
-                        // Entarda de dados do valor1
-                        entradaDados.question('Primeira nota: \n', function (nota1) {
-
-                            let valor1 = nota1.replace(',', '.');
-
-                            //Entrada de dados valor2
-                            entradaDados.question('Segunda nota: \n', function (nota2) {
-
-                                let valor2 = nota2.replace(',', '.');
-
-                                //Entrada de dados valor3
-                                entradaDados.question('Terceira nota: \n', function (nota3) {
-
-                                    let valor3 = nota3.replace(',', '.');
-
-                                    //Entrada de dados valor4
-                                    entradaDados.question('Quarta nota: \n', function (nota4) {
-
-                                        let valor4 = nota4.replace(',', '.');
-
-                                        let resultado;
-
-                                        // Validação de entrada de dados vazios
-                                        if (valor1 == '' || valor2 == '' || valor3 == '' || valor4 == '') {
-                                            console.log('ERRO: Não é possível calcular sem preencher todos os dados!')
-                                            entradaDados.close(); //Fecha o objeto de entrada de dados (emcerra o programa)
-
-                                            //Validação de enrada de dados não numéricos 
-                                        } else if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4)) {
-                                            console.log('ERRO: Não é possível calcular sem a entrada de valores numericos!')
-                                            entradaDados.close(); //Fecha o objeto de entrada de dados (emcerra o programa)
-
-                                        } else if (valor1 < 0 || valor2 < 0 || valor3 < 0 || valor4 < 0) {
-                                            console.log('Erro: Nota não pode ser menor que 0')
-                                            entradaDados.close();
-
-                                        } else if (valor1 > 100 || valor2 > 100 || valor3 > 100 || valor4 > 100) {
-                                            console.log('Erro: Nota não pode ser maior que 100')
-                                            entradaDados.close();
-
-                                        } else {
-                                            resultado = matematica.calcular(valor1, valor2, valor3, valor4)
-
-                                            if (resultado >= 70) {
-                                                console.log('APROVADO')
-    
-                                            } else (resultado < 50) {
-                                                console.log('REPROVADO')
-    
-
-                                                
+    let nomeAluno = aluno
+    if (aluno == ' ') {
+        console.log('ERRO: Digite seu nome!')
+        entradaDados.close()
+    } else {
+        entradaDados.question('Genero do aluno: [FEMININO | MASCULINO] \n', function (sexoAluno) {
+            let sexoAluno1 = sexoAluno
+            if (sexoAluno == '') {
+                console.log('ERRO: Digite o genero do aluno [FEMININO | MASCULINO]')
+                entradaDados.close()
+            } else {
+                entradaDados.question('Nome do Professor: \n', function (professor) {
+                    let nomeProfessor = professor
+                    if (professor == '') {
+                        console.log('ERRO: Não é possível continuar com entrada vazia')
+                        entradaDados.close()
+                    } else {
+                        entradaDados.question('Genero do Professor: [FEMININO | MASCULINO] \n', function (sexoProfessor1) {
+                            let sexoProfessor = sexoProfessor1.toUpperCase()
+                            if (sexoProfessor1 == ' ') {
+                                console.log('ERRO: Não é possível continuar com entrada vazia')
+                                entradaDados.close()
+                            } else if (
+                                sexoProfessor != 'FEMININO' && sexoProfessor != 'MASCULINO') {
+                                console.log('Erro: Por favor escolha entre [MASCULINO | FEMININO ]')
+                                entradaDados.close()
+                            } else {
+                                entradaDados.question('Curso: \n', function (curso) {
+                                    let nomeCurso = curso.toUpperCase()
+                                    if (curso == '') {
+                                        console.log('ERRO: Digite seu curso!')
+                                        entradaDados.close()
+                                    } else {
+                                        entradaDados.question('Disciplina: \n', function (disciplina) {
+                                            let nomeDisciplina = disciplina.toUpperCase()
+                                            if (disciplina == '') {
+                                                console.log('ERRO: Não é possível continuar com entrada vazia')
+                                                entradaDados.close()
                                             }
-                                        
-                                        }
 
-                                    });
+                                            // Entarda de dados do valor1
+                                            entradaDados.question('Primeira nota: \n', function (nota1) {
+
+                                                let valor1 = nota1.replace(',', '.');
+
+                                                //Entrada de dados valor2
+                                                entradaDados.question('Segunda nota: \n', function (nota2) {
+
+                                                    let valor2 = nota2.replace(',', '.');
+
+                                                    //Entrada de dados valor3
+                                                    entradaDados.question('Terceira nota: \n', function (nota3) {
+
+                                                        let valor3 = nota3.replace(',', '.');
+
+                                                        //Entrada de dados valor4
+                                                        entradaDados.question('Quarta nota: \n', function (nota4) {
+
+                                                            let valor4 = nota4.replace(',', '.');
+
+                                                            // Validação de entrada de dados vazios
+                                                            if (valor1 == '' || valor2 == '' || valor3 == '' || valor4 == '') {
+                                                                console.log('ERRO: Não é possível calcular sem preencher todos os dados!')
+                                                                entradaDados.close(); //Fecha o objeto de entrada de dados (emcerra o programa)
+
+                                                                //Validação de enrada de dados não numéricos 
+                                                            } else if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4)) {
+                                                                console.log('ERRO: Não é possível calcular sem a entrada de valores numericos!')
+                                                                entradaDados.close(); //Fecha o objeto de entrada de dados (emcerra o programa)
+
+                                                            } else if (valor1 < 0 || valor2 < 0 || valor3 < 0 || valor4 < 0) {
+                                                                console.log('Erro: Nota não pode ser menor que 0')
+                                                                entradaDados.close();
+
+                                                            } else if (valor1 > 100 || valor2 > 100 || valor3 > 100 || valor4 > 100) {
+                                                                console.log('Erro: Nota não pode ser maior que 100')
+                                                                entradaDados.close();
+
+                                                            } else {
+                                                                let resultado = matematica.calcular(valor1, valor2, valor3, valor4)
+                                                            }
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    }
                                 });
-                            });
+                            }
                         });
-                    });
+                    }
                 });
-            });
+            }
         });
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+})   
--- modulo/sistema.js
+++ modulo/sistema.js
@@ -5,32 +5,78 @@
  * Versão: 1.0
  ******************************************************************************/
 
-const calcular = function(nota1, nota2, nota3,nota4){
+const calcular = function (nota1, nota2, nota3, nota4) {
     let valor1 = Number(nota1);
     let valor2 = Number(nota2);
     let valor3 = Number(nota3);
     let valor4 = Number(nota4);
 
-    let resultado = ((valor1 + valor2 + valor3 + valor4)/4);
+    let resultado = ((valor1 + valor2 + valor3 + valor4) / 4);
 
     console.log(resultado);
 }
- 
-const informacoes = function(aluno, professor,sexoAluno, sexoProfessor,curso, disciplina){
-    let pessoaAluno = aluno;
-    let pessoaProfessor = professor;
-    let sexoAluno1 = sexoAluno;
-    let sexoProfessor1 = sexoProfessor;
-    let nomeCurso = curso;
-    let nomeDisciplina = disciplina;
 
+
+
+function exame(mediaAluno, notaExame) {
+    let resultado = mediaAluno
+    let calcular = notaExame
+
+    let resultadoExame = (Number(resultado) + Number(calcular)) / 2
+
+    return resultadoExame
 }
+
+
+
+function validarNotaExame(resultadototal, sexoAluno) {
+    let sexoAluno1 = sexoAluno;
+    let resultadoFinal = resultadototal;
+
+    if (resultadoFinal >= 60) {
+        if (sexoAluno1 == 'MASCULINO') {
+            return 'APROVADO'
+        } else {
+            return 'APROVADA'
+        }
+    } else {
+        if (sexoAluno1 == 'MASCULINO') {
+            return 'REPROVADO'
+        } else {
+            return 'REPROVADA'
+        }
+    }
+}
+
+
+function validarNota(resultado, sexoAluno) {
+    let resultadoFinal = resultado;
+    let sexoAluno1 = sexoAluno;
+
+    if (resultadoFinal >= 70) {
+        if (sexoAluno1 == 'MASCULINO') {
+            return 'APROVADO'
+        } else {
+            return 'APROVADA'
+        }
+    } else if (resultadoFinal < 50) {
+        if (sexoAluno1 == 'MASCULINO') {
+            return 'REPROVADO'
+        } else {
+            return 'REPROVADA'
+        }
+    } else {
+        return false
+    }
+}
+
+
 
 
 //Exporta uma função para ser utilizada em outro arquivo
 module.exports = {
     calcular,
-    informacoes
+    validarNotaExame,
+    validarNota,
+    exame
 }
-
-// calcular(5,5,5,5)
